9dd22b7aaa905a297fa8d5abb3c73f781e3c7d0a
glubix <merlin.bourge@gmail.com>
2023-05-22 00:25:59 +0200

    push mem and up

--- ia2.py
+++ ia2.py
@@ -67,65 +67,77 @@ def prendre_decision(x, y, obstacle_x, obstacle_y):
 running = True
 clock = pygame.time.Clock()
 iterations = 1  # Nombre d'itérations d'apprentissage supplémentaires
-memory_clear_interval = 1000  # Intervalle pour libérer la mémoire
-memory_clear_counter = 0  # Compteur pour suivre l'intervalle
 
 survived_time = 0  # Temps de survie du personnage
 
-while running:
-    pygame.event.pump()  # Traiter les événements en attente
+# Paramètres pour la sauvegarde et la libération de mémoire
+checkpoint_path = "model_checkpoint.ckpt"
+memory_clear_interval = 1000  # Intervalles d'itération avant de libérer la mémoire
+save_interval = 1000  # Intervalles d'itération avant de sauvegarder le modèle
+iteration_count = 0  # Compteur d'itérations
 
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 running = False
-            elif event.key == pygame.K_s:
-                # Sauvegarder le modèle
-                checkpoint.save(file_prefix=checkpoint_path)
-                print("Modèle sauvegardé.")
 
-    # Gestion des contrôles du personnage
-    action = prendre_decision(personnage_x, personnage_y, obstacle_x, obstacle_y)
-    if action == 0 and personnage_y > 0:
-        personnage_y -= personnage_vitesse
-    elif action == 1 and personnage_y < hauteur - personnage_hauteur:
-        personnage_y += personnage_vitesse
+    for _ in range(iterations):
+        # Gestion des contrôles du personnage
+        action = prendre_decision(personnage_x, personnage_y, obstacle_x, obstacle_y)
+        if action == 0 and personnage_y > 0:
+            personnage_y -= personnage_vitesse
+        elif action == 1 and personnage_y < hauteur - personnage_hauteur:
+            personnage_y += personnage_vitesse
 
-    # Déplacement de l'obstacle
-    obstacle_x -= obstacle_vitesse
-    if obstacle_x < -obstacle_largeur:
-        obstacle_x = largeur
-        obstacle_y = random.randint(0, hauteur - obstacle_hauteur)
+        # Déplacement de l'obstacle
+        obstacle_x -= obstacle_vitesse
+        if obstacle_x < -obstacle_largeur:
+            obstacle_x = largeur
+            obstacle_y = random.randint(0, hauteur - obstacle_hauteur)
 
-    # Collision entre le personnage et l'obstacle
-    if obstacle_x < personnage_x + personnage_largeur and obstacle_x + obstacle_largeur > personnage_x \
-            and obstacle_y < personnage_y + personnage_hauteur and obstacle_y + obstacle_hauteur > personnage_y:
-        # Relancer la partie en réinitialisant les positions
-        personnage_x = 25
-        personnage_y = hauteur // 2 - personnage_hauteur // 2
-        obstacle_x = largeur
-        obstacle_y = random.randint(0, hauteur - obstacle_hauteur)
+        # Collision entre le personnage et l'obstacle
+        if obstacle_x < personnage_x + personnage_largeur and obstacle_x + obstacle_largeur > personnage_x \
+                and obstacle_y < personnage_y + personnage_hauteur and obstacle_y + obstacle_hauteur > personnage_y:
+            # Relancer la partie en réinitialisant les positions
+            personnage_x = 25
+            personnage_y = hauteur // 2 - personnage_hauteur // 2
+            obstacle_x = largeur
+            obstacle_y = random.randint(0, hauteur - obstacle_hauteur)
 
-    # Mise à jour du modèle
-    distance = abs(obstacle_x - personnage_x) / largeur  # Calcul de la distance entre le personnage et l'obstacle
-    reward = get_reward(distance, survived_time)
-    with tf.GradientTape() as tape:
-        etat = np.array([personnage_x / largeur, personnage_y / hauteur, obstacle_x / largeur, obstacle_y / hauteur,
-                         personnage_x / largeur, personnage_y / hauteur])
-        prediction = model(np.expand_dims(etat, axis=0))
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=[reward], logits=prediction))
+        # Mise à jour du modèle
+        distance = abs(obstacle_x - personnage_x) / largeur  # Calcul de la distance entre le personnage et l'obstacle
+        reward = get_reward(distance, survived_time)
+        with tf.GradientTape() as tape:
+            etat = np.array([personnage_x / largeur, personnage_y / hauteur, obstacle_x / largeur, obstacle_y / hauteur,
+                             personnage_x / largeur, personnage_y / hauteur])
+            prediction = model(np.expand_dims(etat, axis=0))
+            loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=[reward], logits=prediction))
 
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-    # Libérer la mémoire à intervalles réguliers
-    memory_clear_counter += 1
-    if memory_clear_counter >= memory_clear_interval:
-        tf.keras.backend.clear_session()
-        gc.collect()
-        memory_clear_counter = 0
+        # Libérer la mémoire à chaque intervalle défini
+        if iteration_count % memory_clear_interval == 0:
+            tf.keras.backend.clear_session()
+            gc.collect()
+            print("Mémoire libérée à l'itération", iteration_count)
+
+        iteration_count += 1
+
+        # Sauvegarder le modèle à chaque intervalle défini
+        if iteration_count % save_interval == 0:
+            checkpoint.save(file_prefix=checkpoint_path)
+            print("Modèle sauvegardé à l'itération", iteration_count)
+
+        # Recharger le modèle mis à jour
+        if iteration_count % save_interval == 0:
+            checkpoint.restore(tf.train.latest_checkpoint(checkpoint_path))
+            print("Modèle rechargé pour amélioration à l'itération", iteration_count)
+
+        # ...
 
     # Effacement de l'écran
     fenetre.fill(BLANC)
